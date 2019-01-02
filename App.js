24d7b3b82f69c700c0e7dcd23f537fe1879e9c99
Spencer Attick <spencer.attick@spencer.attick>
2019-01-01 20:20:53 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,122 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import { Button } from 'react-native-elements';
+import analytics from '@segment/analytics-react-native';
+// import logo from './logo.png';
+
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  componentWillMount() {
+     analytics.setup('BC1V3jcBqQcwb2TNeuTKcXkbA4ujOyzJ', {
+      // Record screen views automatically!
+      // recordScreenViews: true,
+      // Record certain application events automatically!
+      // trackAppLifecycleEvents: true
+    })
+  }
+
+  track1Press() {
+    analytics.track('Skill Gained', {
+      item: 'spider-sense',
+      stength: '75%'
+    })
+  }
+
+  track2Press() {
+    analytics.track('Ally Acquired', {
+      name: 'Ganke Lee',
+      relationship: 'friend'
+    })
+  }
+
+  identifyPress() {
+    analytics.identify('42', {
+      name: 'Miles Morales',
+      email: 'mmorales@midtownhigh.edu'
+    });
+  }
+
+  screenPress() {
+    analytics.screen('Ultimate Comics Spider-Man, Vol. 1', {
+      author: 'Brian Michael Bendis',
+      illustrator: 'Sara Pichelli '
+    })
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+      <Image
+      style={{alignSelf: 'center'}}
+      source={require('./logo.png')}
+      />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.instructions}>{instructions}</Text>
+          <Button
+            buttonStyle={styles.button}
+            onPress={() => this.track1Press()}
+            title="Track 1"
+          />
+          <Button
+            buttonStyle={styles.button}
+            onPress={() => this.track2Press()}
+            title="Track 2"
+          />
+          <Button
+            buttonStyle={styles.button}
+            onPress={() => this.identifyPress()}
+            title="Identify"
+          />
+          <Button
+            buttonStyle={styles.button}
+            onPress={() => this.screenPress()}
+            title="Screen"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    maxHeight: '100%',
+    backgroundColor: '#01376C'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: 'orange',
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    justifyContent: 'space-around',
+    minWidth: '75%',
+    minHeight: '50%'
+  },
+  button: {
+    backgroundColor: '#46B67E',
+  }
+
+});
---
+++ ios/Pods/Target Support Files/Pods-reactNativeTestAppTests/Pods-reactNativeTestAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativeTestAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativeTestAppTests
+@end
